a7fa41733c91ff0b587fe4d0b5d9e33dc2c8af41
Jonas Weirauch <jonas.weirauch@gmx.de>
2025-11-03 21:36:58 +0100

    Refactor configuration loading in conf.rb to improve clarity and maintainability

--- conf.rb
+++ conf.rb
@@ -1,7 +1,19 @@
 require 'sinatra'
 require 'sinatra/config_file'
+require 'yaml'
+require 'data_mapper'
 
 config_file File.join(File.dirname(File.expand_path(__FILE__)), 'config.yaml')
+
+CONFIG_PATH = File.join(File.dirname(__FILE__), 'config.yaml')
+CONFIG = YAML.load_file(CONFIG_PATH)
+
+DB_URI   = CONFIG['db_uri']    || 'sqlite:///./tournament.db'
+API_KEYS = CONFIG['api_keys']  || []
+BIND_HOST = CONFIG['bind_host'] || '0.0.0.0'
+BIND_PORT = (CONFIG['bind_port'] || 4567).to_i
+
+DataMapper.setup(:default, DB_URI)
 
 module Conf
   module_function
